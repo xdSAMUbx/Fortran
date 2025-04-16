44c5ed506c3e95e3596ccf1ecbd8b450f8796cc3
Samuel Calderon <samuelcalderon1202@hotmail.com>
2025-04-16 13:15:13 -0500

    verificacionFactorial

--- FortranSeries/factorial.f90
+++ FortranSeries/factorial.f90
@@ -1,13 +1,13 @@
 PROGRAM factorial
    IMPLICIT NONE
 
-   INTEGER :: n, fac, i
+   INTEGER(8) :: n, fac, i
    WRITE(*,*) 'Ingrese el factorial que desea calcular: '
    READ(*,*) n !El enteo máximo a calcular es 31!
    fac = 1
-   DO i = 1, n
+   DO i = 1, n, 1
       fac = fac * i
+      WRITE(*,*) 'El valor para ',i,' es: ',fac
    END DO
-   WRITE(*,*) 'El valor para ',i,' es: ',fac
 
 END PROGRAM factorial
