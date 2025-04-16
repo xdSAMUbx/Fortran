5b04c161377eb58c615928df3d30ed737a59c23a
Samuel Calderon <samuelcalderon1202@hotmail.com>
2025-04-16 12:46:42 -0500

    corFactorial

--- FortranSeries/factorial.f90
+++ FortranSeries/factorial.f90
@@ -1,18 +1,13 @@
 PROGRAM factorial
    IMPLICIT NONE
 
-   INTEGER :: n, fac, i, count_start,count_end,count_rate
-   REAL :: tiempo
+   INTEGER :: n, fac, i
    WRITE(*,*) 'Ingrese el factorial que desea calcular: '
    READ(*,*) n !El enteo máximo a calcular es 31!
    fac = 1
-   CALL system_clock(count_start)  ! Inicia cronómetro
    DO i = 1, n
       fac = fac * i
    END DO
-   CALL system_clock(count_end)    ! Finaliza cronómetro
-   CALL system_clock(count_rate)
    WRITE(*,*) 'El valor para ',i,' es: ',fac
-   PRINT *, 'Tiempo de ejecución (segundos): ', tiempo
 
 END PROGRAM factorial
